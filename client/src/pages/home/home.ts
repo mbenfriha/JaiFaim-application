cddb6314f418a268639863b956578074eaa9ee73
mbenfriha <benfriha.mohamed@gmail.com>
2017-09-06 09:04:41 +0200

    add icon & splash

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map'
-import { StatusBar } from '@ionic-native/status-bar';
 import { LaunchNavigator } from '@ionic-native/launch-navigator';
 
 declare var google;
@@ -25,62 +24,67 @@ export class HomePage {
   private platform:Platform = null;
 
   image = "http://www.freeiconspng.com/uploads/map-navigation-pin-point-restaurant-icon--14.png";
-  constructor(public navCtrl: NavController, public geolocation: Geolocation, public http: Http, platform: Platform, private statusBar: StatusBar, private launchNavigator: LaunchNavigator) {
+
+  constructor(public geolocation: Geolocation,
+              public http: Http, platform: Platform,
+              private launchNavigator: LaunchNavigator) {
     this.platform = platform;
 
   }
 
 
   openGps() {
-    console.log('gps');
     let destination = this.lat + ',' + this.lng;
 
     this.launchNavigator.navigate(destination);
-
-    /*if(this.platform.is('ios')){
-      window.open('maps://?q=' + destination, '_system');
-    } else {
-      let label = encodeURI('My Label');
-      window.open('geo:0,0?q=' + destination + '(' + label + ')', '_system');
-    }*/
   }
 
-  searchCook() {
-    this.search = true;
+  doRefresh(e) {
+    this.searchCook();
+    setTimeout(() => {
+      e.complete();
+    }, 2000);
+  }
 
+  getPos() {
+    this.search = true;
     this.geolocation.getCurrentPosition().then((position) => {
       this.myLat = position.coords.latitude;
       this.myLng = position.coords.longitude;
-      this.http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+this.myLat+','+ this.myLng+'&radius=200&type=restaurant&key='+this.token).map(res => res.json()).subscribe(data => {
-
-        let resultCook = data.results[Math.floor(Math.random()*data.results.length)];
-
-        this.http.get('https://maps.googleapis.com/maps/api/place/details/json?placeid='+resultCook.place_id+'&key='+this.token).map(res => res.json()).subscribe(data2 => {
-
-          resultCook = data2.result;
-          console.log(resultCook);
-          this.lat = resultCook.geometry.location.lat;
-          this.lng = resultCook.geometry.location.lng;
-          console.log(this.lat, this.lng);
-
-          this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins='+position.coords.latitude+','+ position.coords.longitude +'&destinations='+resultCook.geometry.location.lat+','+ resultCook.geometry.location.lng +'&key=AIzaSyBR9qbn52JXC_AKne3F01Z2JjeKCYpcB64').map(res => res.json()).subscribe(data3 => {
-            this.distance = data3.rows[0].elements[0].distance.value;
-          })
-
-          if(resultCook.photos) {
-            this.http.get('https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + resultCook.photos[0].photo_reference + '&key='+this.token).map(res => res).subscribe(data => {
-              this.search = false;
-              this.result = {result: resultCook, image: data.url}
-            })
-          }
-          else {
-            this.search = false;
-            this.result = {result: resultCook}
-          }
-        })
-      });
+      this.searchCook();
+      return true;
     }, (err) => {
       console.log(err);
+    });
+  }
+
+  searchCook() {
+    this.http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + this.myLat + ',' + this.myLng + '&radius=200&type=restaurant&key=' + this.token).map(res => res.json()).subscribe(data => {
+
+      let resultCook = data.results[Math.floor(Math.random() * data.results.length)];
+
+      this.http.get('https://maps.googleapis.com/maps/api/place/details/json?placeid=' + resultCook.place_id + '&key=' + this.token).map(res => res.json()).subscribe(data2 => {
+
+        resultCook = data2.result;
+        this.lat = resultCook.geometry.location.lat;
+        this.lng = resultCook.geometry.location.lng;
+
+        this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=' + this.myLat + ',' + this.myLng + '&destinations=' + resultCook.geometry.location.lat + ',' + resultCook.geometry.location.lng + '&key=AIzaSyBR9qbn52JXC_AKne3F01Z2JjeKCYpcB64').map(res => res.json()).subscribe(data3 => {
+          this.distance = data3.rows[0].elements[0].distance.value;
+        })
+
+        if (resultCook.photos) {
+          this.http.get('https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + resultCook.photos[0].photo_reference + '&key=' + this.token).map(res => res).subscribe(data => {
+            this.search = false;
+            this.result = {result: resultCook, image: data.url}
+          })
+        }
+        else {
+          this.search = false;
+          this.result = {result: resultCook}
+        }
+        return true;
+      })
     });
   }
 
